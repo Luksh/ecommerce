8300d5ab7ab975f55aa627fce493038aadad38f5
Luksh <hotmale_luksh@hotmail.com>
2021-12-23 11:26:43 +0545

    third commit

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,9 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('', HomeView.as_view(), name = 'home'),
-    path('category/<slug>', CategoryView.as_view)
+    path('', HomeView.as_view(),name = 'home'),
+    path('category/<slug>', CategoryView.as_view(), name = 'category'),
+    path('product/<slug>', DetailView.as_view(), name = 'product'),
+    path('search', SearchView.as_view(), name = 'search'),
+    path('signup', signup, name = 'signup'),
 ]
